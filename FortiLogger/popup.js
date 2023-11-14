d626469b92bf5a1497a620b0d75318d67894546c
FazlOmar9 <bt22csa026@iiitn.ac.in>
2023-11-14 22:20:11 +0530

    Fixed empty field bug, username field value, new logo

--- FortiLogger/popup.js
+++ FortiLogger/popup.js
@@ -4,3 +4,10 @@ document.getElementById('credentialsForm').addEventListener('submit', e => {
   var password = document.getElementById('password').value;
   chrome.storage.sync.set({username: username, password: password}, () => window.close());
 });
+
+
+chrome.storage.sync.get('username', data => {
+  if (data.username) {
+    document.getElementById('username').value = data.username;
+  }
+});
