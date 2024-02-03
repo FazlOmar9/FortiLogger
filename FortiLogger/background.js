1e21f682b907e2cb6ca29ff5d640dc708869e454
FazlOmar9 <bt22csa026@iiitn.ac.in>
2024-02-03 13:56:40 +0530

    Fixed V3 versioning bug

--- FortiLogger/background.js
+++ FortiLogger/background.js
@@ -1,4 +1,4 @@
-chrome.browserAction.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener((tab) => {
   if(tab.url.startsWith('http://192.168.55.253') || tab.url.startsWith('https://192.168.55.253')) {
     chrome.tabs.executeScript(tab.id, {file: "contents.js"});
   }
